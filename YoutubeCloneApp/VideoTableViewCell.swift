537ed3a5d1b2cb101fc830dec72a476f2b7a3fed
muratyildirimm <muratyildirimm495@gmail.com>
2023-05-25 01:51:07 +0300

    app almost done

--- YoutubeCloneApp/VideoTableViewCell.swift
+++ YoutubeCloneApp/VideoTableViewCell.swift
@@ -14,13 +14,33 @@ class VideoTableViewCell: UITableViewCell {
   
   override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
+  
+  func prepare(video: Video) {
+    print(video.title)
+    tagLabel.text = video.tag
+    titleLabel.text = video.title
+    descriptionLabel.text = video.shortDescription
+    img.load(url: video.imageUrl)
+  }
 
+}
+
+extension UIImageView {
+  func load(url: URL) {
+    DispatchQueue.global().async { [weak self] in
+      if let data = try? Data(contentsOf: url) {
+        if let image = UIImage(data: data) {
+          DispatchQueue.main.async {
+            self?.image = image
+          }
+        }
+      }
+    }
+  }
 }
